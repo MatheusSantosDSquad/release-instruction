908191ff67afef6ba5b465a17590b8f348eb638d
Matheus Lopes Santos <matheussantos@devsquad.com>
2022-01-12 16:41:37 -0300

    Added api route for something
    Implements: #9

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/api/something', function() {
+    return response()->json('Yeap! success');
+});
